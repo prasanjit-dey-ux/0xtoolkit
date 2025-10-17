82b80d4cba800115fe36024c1ff9c29b163974de
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-17 13:16:09 +0530

    updated next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <ThemeProvider  attribute="class" defaultTheme="system" enableSystem>
       <main className="bg-background text-foreground h-screen">
           {children}
       </main>
+      </ThemeProvider>
       </body>
     </html>
   );
