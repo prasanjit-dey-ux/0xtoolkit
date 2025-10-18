24ba549ae2007fc66169d024bca90ca5355b777f
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-18 22:33:58 +0530

    added a manhwa bento

---
+++ app/components/bento.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export const Bento = () => {
+  return (
+ <div className='max-w-5xl mx-auto min-h-screen border-x border-neutral-400 bg-gray-100 selection:bg-pink-600'>
+        <div className='h-20 border-b border-neutral-400 justify-content'>    
+        </div>
+
+        <h1 className='p-4 font-primary font-black text-8xl flex justify-start text-center items-center text-neutral-950 '>
+            WIND BREAKERS
+        </h1>
+
+        <div className='p-4 grid grid-cols-3 gap-4 border-y border-neutral-400'>
+            <div className='bg-neutral-200 h-80 w-full shrink-0 border border-neutral-400 col-span-1 bg-[url("https://i.pinimg.com/1200x/6a/8e/ec/6a8eec0c734b7f873141b54dc48e95d4.jpg")] bg-cover'></div>
+            <div className='bg-neutral-200 h-80 w-full shrink-0 border border-neutral-400 col-span-2 bg-[url("https://i.pinimg.com/1200x/b7/3b/32/b73b3292dbf4928923a6d51113f27609.jpg")] bg-cover'></div>
+            <div className='bg-neutral-200 h-80 w-full shrink-0 border border-neutral-400 col-span-2 bg-[url("https://i.pinimg.com/1200x/46/d0/8d/46d08d53abc317964c7dafe4e1796d03.jpg")] bg-cover'></div>
+            <div className='bg-neutral-200 h-80 w-full shrink-0 border border-neutral-400 col-span-1 bg-[url("https://i.pinimg.com/736x/cb/c7/78/cbc778193f1fbb6ac13e527bc27dfb1a.jpg")] bg-cover'></div>
+        </div>
+                <h1 className='p-4 font-primary font-black text-8xl flex justify-start text-center items-center text-neutral-950 '>
+            WIND BREAKERS
+        </h1>
+
+        <div className='p-4 grid grid-cols-3 gap-4 border-y border-neutral-400'>
+            <div className='bg-neutral-200 h-80 w-full shrink-0 border border-neutral-400 col-span-1 bg-[url("https://i.pinimg.com/1200x/6a/8e/ec/6a8eec0c734b7f873141b54dc48e95d4.jpg")] bg-cover'></div>
+            <div className='bg-neutral-200 h-80 w-full shrink-0 border border-neutral-400 col-span-2 bg-[url("https://i.pinimg.com/1200x/b7/3b/32/b73b3292dbf4928923a6d51113f27609.jpg")] bg-cover'></div>
+            <div className='bg-neutral-200 h-80 w-full shrink-0 border border-neutral-400 col-span-2 bg-[url("https://i.pinimg.com/1200x/46/d0/8d/46d08d53abc317964c7dafe4e1796d03.jpg")] bg-cover'></div>
+            <div className='bg-neutral-200 h-80 w-full shrink-0 border border-neutral-400 col-span-1 bg-[url("https://i.pinimg.com/736x/cb/c7/78/cbc778193f1fbb6ac13e527bc27dfb1a.jpg")] bg-cover'></div>
+        </div>
+        
+    </div>
+  )
+}
