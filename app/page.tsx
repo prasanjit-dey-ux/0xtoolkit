509a656bb66957a7bddfcb01178c3efdd4c33374
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-19 03:40:18 +0530

    updated components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-{/*import { Card } from "./components/card"; */}
-import { ModeToggle } from "./components/mode-toggle";
+// import { Card } from "./components/card";
+// import { ModeToggle } from "./components/modeToggle";
+
+import { Grid } from "./components/grid";
 
 export default function Home() {
   return (
     <div className="">
-      {/*<Card />*/}
-      <ModeToggle />
+      {/* <Card/> */}
+      {/* <ModeToggle /> */}
+      <Grid />
     </div>
   );
 }
