42d58f7c2a27e807321e24b8d6a300505e93303d
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-20 20:57:51 +0530

    added onboarding

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SelectedTrackingSources } from './selectedTrackingSources'
+
+export default function Onboarding(){
+  return (
+    <div className='bg-[#F7F7F7]'>
+    <SelectedTrackingSources/>
+    </div>
+  )
+}
