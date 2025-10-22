4009394e1a65d568f6f1560b8b4e7110aeb4da66
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-22 22:50:54 +0530

    added color layer

--- ui/card.tsx
+++ ui/card.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import clsx from 'clsx'
 
-export const Hovercard = () => {
+export const Hovercard = ({className}: {className?:string}) => {
   return (
     <div className={clsx("bg-[radial-gradient(var(--color-neutral-200)_1px,_transparent_1px)]",
         "[background-size:10px_10px]",
         "size-50 border border-neutral-200 rounded-2xl bg-neutral-100 group-hover:bg-neutral-200",
-        "shadow-lg relative perspective-distant transition-colors duration-300 delay-300"
+        "shadow-lg relative perspective-distant transition-colors duration-300 delay-300",
+        className
     )}
     >    
       <img src="https://i.pinimg.com/736x/30/c0/3b/30c03b076f9045ec09b39e6607bffc15.jpg" alt=""
@@ -20,4 +21,34 @@ export const Hovercard = () => {
       />
     </div>
   )
+}
+
+export const PolarCard = () => {
+      return <div className='flex justify-center items-center grid-col-2 size-60 px-2 bg-white border border-neutral-200 overflow-hidden'>
+        <div className='border border-neutral-200 w-full h-60 p-2'>
+                  <div className='border border-neutral-200 bg-blue-50 w-full h-60 p-2'>
+                  <div className='border border-neutral-200 bg-blue-50 w-full h-60 p-2'>
+                  <div className='border border-neutral-200 bg-blue-100 w-full h-60 p-2'>
+                  <div className='border border-neutral-200 bg-blue-200 w-full h-60 p-2'>
+                  <div className='border border-neutral-200 bg-blue-300 w-full h-60 p-2'>
+                  <div className='border border-neutral-200 bg-blue-400 w-full h-60 p-2'>
+                  <div className='border border-neutral-200 bg-blue-500 w-full h-60 p-2'>
+                  <div className='border border-neutral-200 bg-blue-600 w-full h-60 p-2'>
+                  <div className='border border-neutral-200 bg-blue-700 w-full h-60 p-2'>
+                  <div className='border border-neutral-200 bg-blue-800 w-full h-60 p-2'>
+                  <div className='border border-neutral-200 bg-blue-900 w-full h-60 p-2'>
+          
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+      </div>
 }
