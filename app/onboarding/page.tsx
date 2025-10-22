34828bec77ef57b3e009bf69dde4d71958a97d3b
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-22 21:29:58 +0530

    added ConfirmYourCompitiors

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { SelectedTrackingSources } from './selectedTrackingSources'
+import { ConfirmYourCompetitors } from './confirmYourCompetitors'
 
 export default function Onboarding(){
   return (
     <div className='bg-[#F7F7F7]'>
     <SelectedTrackingSources/>
+    <ConfirmYourCompetitors />
     </div>
   )
 }
