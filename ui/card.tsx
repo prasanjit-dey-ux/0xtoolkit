2d2fe8c46c828505294165bc5a7cae30b852766d
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-23 22:58:15 +0530

    updated the hovercard animation: reduced delay

--- ui/card.tsx
+++ ui/card.tsx
@@ -12,7 +12,7 @@ export const Hovercard = ({className}: {className?:string}) => {
     >    
       <img src="https://i.pinimg.com/736x/30/c0/3b/30c03b076f9045ec09b39e6607bffc15.jpg" alt=""
       className={clsx(
-        'transition-transform delay-300 [transition-timing-function:cubic-bezier(.4,0,.2,1)]',
+        'transition-transform delay-200 [transition-timing-function:cubic-bezier(.4,0,.2,1)]',
         'object-cover rounded-2xl h-full w-full object-bootom border border-neutral-200',
         'transform rotate-x-30 rotate-z-30 -rotate-y-30 translate-z-20',
         'group-hover:rotate-x-0 group-hover:rotate-z-0 group-hover:rotate-y-0 group-hover:scale-85'
@@ -26,17 +26,17 @@ export const Hovercard = ({className}: {className?:string}) => {
 export const PolarCard = () => {
       return <div className='flex justify-center items-center grid-col-2 size-60 px-2 bg-white border border-neutral-200 overflow-hidden'>
         <div className='border border-neutral-200 w-full h-60 p-2'>
-                  <div className='border border-neutral-200 bg-blue-50 w-full h-60 p-2'>
-                  <div className='border border-neutral-200 bg-blue-50 w-full h-60 p-2'>
-                  <div className='border border-neutral-200 bg-blue-100 w-full h-60 p-2'>
-                  <div className='border border-neutral-200 bg-blue-200 w-full h-60 p-2'>
-                  <div className='border border-neutral-200 bg-blue-300 w-full h-60 p-2'>
-                  <div className='border border-neutral-200 bg-blue-400 w-full h-60 p-2'>
-                  <div className='border border-neutral-200 bg-blue-500 w-full h-60 p-2'>
-                  <div className='border border-neutral-200 bg-blue-600 w-full h-60 p-2'>
-                  <div className='border border-neutral-200 bg-blue-700 w-full h-60 p-2'>
-                  <div className='border border-neutral-200 bg-blue-800 w-full h-60 p-2'>
-                  <div className='border border-neutral-200 bg-blue-900 w-full h-60 p-2'>
+                  <div className='hover:border border-neutral-200 bg-blue-50 w-full h-60 p-2'>
+                  <div className='hover:border border-neutral-200 bg-blue-50 w-full h-60 p-2'>
+                  <div className='hover:border border-neutral-200 bg-blue-100 w-full h-60 p-2'>
+                  <div className='hover:border border-neutral-200 bg-blue-200 w-full h-60 p-2'>
+                  <div className='hover:border border-neutral-200 bg-blue-300 w-full h-60 p-2'>
+                  <div className='hover:border border-neutral-200 bg-blue-400 w-full h-60 p-2'>
+                  <div className='hover:border border-neutral-200 bg-blue-500 w-full h-60 p-2'>
+                  <div className='hover:border border-neutral-200 bg-blue-600 w-full h-60 p-2'>
+                  <div className='hover:border border-neutral-200 bg-blue-700 w-full h-60 p-2'>
+                  <div className='hover:border border-neutral-200 bg-blue-800 w-full h-60 p-2'>
+                  <div className='hover:border border-neutral-200 bg-blue-900 w-full h-60 p-2'>
           
         </div>
         </div>
