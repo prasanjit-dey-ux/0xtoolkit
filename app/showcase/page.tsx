9639b0d25761889b868a5b6b9e62d99eb0600fc6
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-23 22:56:50 +0530

    updated card to cardbox

--- app/showcase/page.tsx
+++ app/showcase/page.tsx
@@ -3,9 +3,8 @@ import React from 'react'
 import clsx from 'clsx';
 import { Btn1, ToggleBtn } from '@/ui/button1';
 import { Hovercard, PolarCard } from '@/ui/card';
-import { Car } from 'lucide-react';
 
-const Card = ({children, className}: {children:React.ReactNode, className?: string}) => {
+export const CardBox= ({children, className}: {children:React.ReactNode, className?: string}) => {
     return <div className={clsx('bg-white h-80 flex justify-center items-center',
         "bg-[radial-gradient(var(--color-neutral-100)_1px,_transparent_1px)]",
         "[background-size:10px_10px]",
@@ -19,22 +18,19 @@ export default function ShowCase() {
   return (
     <div className='max-w-5xl mx-auto min-h-screen bg-gray-100 border-x border-neutral-200 '>
         <div className='grid grid-cols-2 divide-x divide-y divide-neutral-200'>
-            <Card>
+            <CardBox>
                 <Btn1>Continue</Btn1>
-            </Card>
-            <Card>
+            </CardBox>
+            <CardBox>
                 <ToggleBtn />   
-            </Card>
-            <Card className='group'>
+            </CardBox>
+            <CardBox className='group'>
                  <Hovercard />   
-            </Card>
-            <Card>
+            </CardBox>
+            <CardBox>
                 <PolarCard />    
-            </Card>  
-        </div>
-        <div className='grid grid-cols-3'>
-            
-        </div>
+            </CardBox>
+        </div> 
     </div>
   )
 }
