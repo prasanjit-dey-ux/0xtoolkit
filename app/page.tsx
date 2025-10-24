12cc4992e11bce1440301f9f06b367314a6b4370
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-24 10:33:27 +0530

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 // import { Card } from "./components/card";
 // import { ModeToggle } from "./components/modeToggle";
 
-import { Hovercard } from "@/ui/card";
-import { Card } from "./components/card";
-import { CardBox } from "./showcase/page";
-// import { Grid } from "./components/grid";
+// import { Hovercard } from "@/ui/card";
+// import { Card } from "./components/card";
+// import { CardBox } from "./showcase/page";
+import { Grid } from "./components/grid";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       {/* <Card/> */}
       {/* <ModeToggle /> */}
-      {/* <Grid /> */}
-      <CardBox className='group border border-neutral-200 w-80 rounded-2xl'>
+      <Grid />
+      {/* <CardBox className='group border border-neutral-200 w-80 rounded-2xl'>
         <Hovercard />
-      </CardBox>
+      </CardBox> */}
     </div>
   );
 }
