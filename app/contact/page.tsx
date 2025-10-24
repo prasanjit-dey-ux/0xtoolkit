0cf93e48bbcefe4a615575ffd543a1995f56bc61
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-24 23:58:42 +0530

    update the InputImage component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,14 @@
 "use client";
-import React from 'react'
+import React, { useState } from 'react'
 import clsx from 'clsx'
 import { CardBox } from '../showcase/page';
+import Image from 'next/image';
 
 
 export default function Contact(){
+    
+    const [profileImage, setProfileImage] = useState('');
+
     return <div className='max-w-7xl h-screen bg-neutral-100 mx-auto'>
         <div className='grid grid-cols-2 divide-x divide-neutral-200 '>
             <form className='bg-neutral-50 h-screen p-12'>
@@ -28,9 +32,26 @@ export default function Contact(){
                         placeholder='Enter your display name'
                         />
                 </Group>
+                <Group>
+                    <Label>Profile Image</Label>
+                    <ProfileImageCard
+                    value={profileImage}
+                    onChange={e=> setProfileImage(e.target.value)}
+                    />              
+                    
+                </Group>
+                <Group>
+                    <Label>Banner Image</Label>
+                </Group>
+                <ImageInputCard>
+                    <Button></Button>
+                </ImageInputCard>
                 <Button>Submit</Button>                          
                 </div>
             </form>
+
+
+
             <div className={clsx(
                 'bg-white h-screen flex justify-center items-center',
                 'bg-[radial-gradient(var(--color-neutral-300)_1px,transparent_1px)]',
@@ -54,19 +75,16 @@ const Label = ({children, className, ...props}:{children: React.ReactNode, class
 
 const Input = ({ className,...props }: React.InputHTMLAttributes<HTMLInputElement>) => {
     return (
-                    <input 
-                    name='Email'
-                    type='text'
-                    placeholder='Enter your email'
-                    className={clsx(
-                        'shadow-input bg-white px-4 py-2 rounded-md border border-transparent',
-                        'focus:outline-none focus:border-gray-300 focus:bg-gray-100 focus:ring-2 focus:ring-gray-300  focus:ring-offset-2 ',
-                        'will-change-transform duration-200',
-                        'placeholder:text-neutral-400',
-                        className
-                    )} 
-                    {...props}
-                    />
+        <input 
+            className={clsx(
+                'shadow-input bg-white px-4 py-2 rounded-md border border-transparent',
+                'focus:outline-none focus:border-gray-300 focus:bg-gray-100 focus:ring-2 focus:ring-gray-300  focus:ring-offset-2 ',
+                'will-change-transform duration-200',
+                'placeholder:text-neutral-400',
+                 className
+            )} 
+                {...props}
+        />
 
     )
 }
@@ -77,12 +95,78 @@ const Group = ({children}:{children:React.ReactNode}) => {
     </div>
 }
 
-const Button = ({children}:{children:React.ReactNode}) => {
-    return <button className={clsx('w-full px-4 py-2 bg-[#FF5A4D] rounded-md text-white hover:bg-[#f74235] cursor-pointer drop-shadow-sm',
-        'transitio-all duration-150 ease-out hover:-translate-y-0.5 active:scale-98'
+const Button = ({children, className}:{children:React.ReactNode, className?:string}) => {
+    return <button className={clsx('px-4 py-2 bg-[#FF5A4D] rounded-md text-white hover:bg-[#f74235] cursor-pointer drop-shadow-sm',
+        'transitio-all duration-150 ease-out hover:-translate-y-0.5 active:scale-98',
+        className
     )}>
         {children}
     </button>
+}
+
+
+const ProfileImageCard = ({ value, onChange }: { value: string; onChange: React.ChangeEventHandler<HTMLInputElement> }) => {
+  return (
+    <div className="flex items-center gap-2">
+      <img
+        src={value || '/profile.gif'}
+        alt="Profile Image"
+        height={88}
+        width={88}
+        className="rounded-full object-cover w-20 h-20"
+      />
+      <Input
+        name="profileImage"
+        placeholder="Paste your image url"
+        value={value}
+        onChange={onChange}
+        type="url"
+        className="flex-1"
+      />
+      <Button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-clipboard-paste-icon lucide-clipboard-paste"><path d="M11 14h10"/><path d="M16 4h2a2 2 0 0 1 2 2v1.344"/><path d="m17 18 4-4-4-4"/><path d="M8 4H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h12a2 2 0 0 0 1.793-1.113"/><rect x="8" y="2" width="8" height="4" rx="1"/></svg></Button>
+    </div>
+  );
+};
+
+type ImageInputProps = {
+    value: string;
+    onChange: React.ChangeEventHandler<HTMLInputElement>;
+    layout?: 'row' | 'column';
+    inputName?: string;
+    inputPlaceholder?: string;
+    defaultImage?: string;
+    imgClassName?: string;
+    children?: React.ReactNode;
+}
+
+const ImageInputCard = ({
+    value,
+    onChange,
+    layout = "row",
+    inputName = "imageUrl",
+    inputPlaceholder = "Paste your image url",
+    defaultImage = '/profile.gif',
+    imgClassName = 'rounded-full w-20 h-20',
+    children,
+}: ImageInputProps) => {
+    
+    if( layout === 'row') {
+        return (
+            <div className='flex item-center gap-2'>
+                <img 
+                    src={value || defaultImage}
+                    alt="Image Preveiw"
+                    className={`object-cover ${imgClassName}`}
+                />
+                <Input
+                    name={inputName}
+                    placeholder=''
+                />
+            </div>
+        )
+    }
+
+
 }
 
 
