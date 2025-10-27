9b5b427544fa50ce42849437f3080f7951bb479c
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-27 23:01:54 +0530

    add svg

---
+++ app/svg-components/page.tsx
@@ -0,0 +1,13 @@
+
+import { SVGComponent } from "./svg1"
+
+export default function Page() {
+    return(
+        <>
+            <div className="bg-gray-50 h-screen max-w-4xl mx-auto flex flex-col items-center py-20">
+                <SVGComponent />
+            </div>
+
+        </>
+    )
+}
