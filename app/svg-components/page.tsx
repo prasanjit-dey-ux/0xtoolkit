9b4e53607dcbe623824c8cdb1b6ebc858d51a0cb
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-29 08:13:42 +0530

    added svg line in page

--- app/svg-components/page.tsx
+++ app/svg-components/page.tsx
@@ -1,10 +1,16 @@
 
-import { SVGLines } from "./svg-lines"
+import clsx from "clsx"
+import { SVGLines, SVGTree } from "./svg-lines"
 
 export default function Page() {
     return(
         <>
-            <div className="bg-gray-100 h-screen max-w-4xl mx-auto flex flex-col items-center justify-center">
+            <div className={clsx(
+                "bg-white h-screen max-w-4xl mx-auto flex flex-col items-center justify-center px-60",
+                "bg-[radial-gradient(var(--color-neutral-100)_1px,_transparent_1px)]",
+        "[background-size:10px_10px]",
+
+            )}>
                 <SVGLines />
             </div>
 
