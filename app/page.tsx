41b7cd615151687eb24009ec1134fcb96c3353d4
Prasanjit Dey <prasanjitdey8523@gmail.com>
2025-10-31 17:29:17 +0530

    added statCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
-// import { Card } from "./components/card";
-// import { ModeToggle } from "./components/modeToggle";
-
-// import { Hovercard } from "@/ui/card";
-// import { Card } from "./components/card";
-// import { CardBox } from "./showcase/page";
-import { Grid } from "./components/grid";
+import { StatCard } from "./components/statCard";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
-      {/* <Card/> */}
-      {/* <ModeToggle /> */}
-      <Grid />
-      {/* <CardBox className='group border border-neutral-200 w-80 rounded-2xl'>
-        <Hovercard />
-      </CardBox> */}
+        <StatCard />
     </div>
   );
 }
